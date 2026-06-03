1afafcdfe35e6a039034e865b37d779dfec917ac
rafaaaarruda <rafacruzarrud@gmail.com>
2026-06-03 11:22:46 -0700

    Initial Commit

---
+++ auau/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  auau
+//
+//  Created by academy on 03/06/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ auau/auauApp.swift
@@ -0,0 +1,17 @@
+//
+//  auauApp.swift
+//  auau
+//
+//  Created by academy on 03/06/26.
+//
+
+import SwiftUI
+
+@main
+struct auauApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
